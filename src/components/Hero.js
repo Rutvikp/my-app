f3618e57c8cb85683dff2787455725ce71ac1a8a
wu-jeffrey <j387wu@edu.uwaterloo.ca>
2020-05-29 11:19:43 -0400

    You're welcome

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,28 @@
-import React, {useState} from "react";
-import {Container, Jumbotron} from "react-bootstrap";
-import Fade from "react-bootstrap/Fade";
+import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import '../index.css';
-import CSSTransition from "react-transition-group/CSSTransition";
+import "../index.css";
+import FadeIn from "./animations/FadeIn.js";
 
-
-function Hero(props){
-    const[inProp] = useState(true);
-// Need to format to a jumbotron from react
-    return(
-        <CSSTransition in={inProp} timeout={200} classNames="my-node">
-            <Jumbotron classNames="bg-transparent">
-            <Container>
-                <Row>
-                    <Col>
-                        { props.title && <h1>{props.title}</h1>}
-                        { props.subTitle && <h5>{props.subTitle}</h5>}
-                        { props.text && <h6>{props.text}</h6>}
-                    </Col>
-                </Row>
-            </Container>
-            </Jumbotron>
-        </CSSTransition>
-    );
-
+function Hero({ title, subTitle, text }) {
+  // Need to format to a jumbotron from react
+  return (
+    <FadeIn>
+      <Jumbotron className="bg-transparent">
+        <Container>
+          <Row>
+            <Col>
+              {title && <h1>{title}</h1>}
+              {subTitle && <h5>{subTitle}</h5>}
+              {text && <h6>{text}</h6>}
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+    </FadeIn>
+  );
 }
 
 export default Hero;
---
+++ src/components/animations/FadeIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
+import "./FadeIn.css";
+
+export default function FadeIn({ children }) {
+  return (
+    <CSSTransition in appear timeout={500} classNames="fade">
+      {children}
+    </CSSTransition>
+  );
+}
